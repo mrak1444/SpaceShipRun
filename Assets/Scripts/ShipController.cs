c28efbea58c486f8158b7343d20ccbb38c98e9c0
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-04-01 18:30:19 +0600

    Собираю уровень

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Characters
+{
+    public class ShipController : MonoBehaviour
+    {
+        public string PlayerName;
+    }
+}
+
