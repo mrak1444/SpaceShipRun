33ae7ff153468428730ce7bfe0109615869578b5
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-04-04 16:44:06 +0600

    Переделал ДЗ к лекции 5 , попытался выполнить задание 1
    Переделал ДЗ к лекции 5 , попытался выполнить задание 1, но что то у меня не получилось. Так как на это задание потратил много времени, заканчиваю его делать приступаю к следующей лекции

--- Assets/Scripts/Main/SolarSystemNetworkManager.cs
+++ Assets/Scripts/Main/SolarSystemNetworkManager.cs
@@ -1,4 +1,6 @@
 using Characters;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -8,50 +10,79 @@ namespace Main
     [System.Obsolete]
     public class SolarSystemNetworkManager : NetworkManager
     {
-        //[SerializeField] private string playerName;
-        [SerializeField] private GameObject _menu;
-        [SerializeField] private InputField _playerName;
-        [SerializeField] private Text _nameText;
+        [SerializeField] private InputField playerName;
+        [SerializeField] private GameObject _textObj;
+
+        private Dictionary<int, ShipController> _players = new Dictionary<int, ShipController>();
+
+        
 
         public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
         {
             var spawnTransform = GetStartPosition();
 
             var player = Instantiate(playerPrefab, spawnTransform.position, spawnTransform.rotation);
-            player.GetComponent<ShipController>().PlayerName = _playerName.text;
-            _nameText.text = player.GetComponent<ShipController>().PlayerName;
+            //player.GetComponent<ShipController>().PlayerName = playerName.text;
+            _players.Add(conn.connectionId, player.GetComponent<ShipController>());
             NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
         }
 
-        public override void OnStartHost()
+        public override void OnClientConnect(NetworkConnection conn)
         {
-            _menu.SetActive(false);
+            base.OnClientConnect(conn);
+            Msggg n = new Msggg();
+            n.Name = playerName.text;
+            conn.Send(100, n);
+            Debug.Log(1);
+        }
+
+        /*public override void OnStartHost()
+        {
+            _textObj.SetActive(false);
+        }*/
+
+        private void ReceiveName(NetworkMessage netMsg)
+        {
+            ShipController shipController = _players[netMsg.conn.connectionId];
+            var nameMessage = netMsg.ReadMessage<Msggg>();
+            shipController.PlayerName = nameMessage.Name;
+            Debug.Log(nameMessage.Name);
         }
 
         public override void OnStartServer()
         {
             base.OnStartServer();
-            _menu.SetActive(false);
+
+            _textObj.SetActive(false);
+
+            NetworkServer.RegisterHandler(100, ReceiveName);
         }
 
         public override void OnStartClient(NetworkClient client)
         {
-            _menu.SetActive(false);
+            _textObj.SetActive(false);
         }
 
         public override void OnStopHost()
         {
-            _menu.SetActive(true);
+            _textObj.SetActive(true);
         }
 
         public override void OnStopServer()
         {
-            _menu.SetActive(true);
+            _textObj.SetActive(true);
         }
 
         public override void OnStopClient()
         {
-            _menu.SetActive(true);
+            _textObj.SetActive(true);
         }
+
+    }
+
+    [System.Obsolete]
+    public class Msggg : MessageBase
+    {
+        public string Name;
     }
 }
--- Assets/Scripts/UI/PlayerLabel.cs
+++ Assets/Scripts/UI/PlayerLabel.cs
@@ -6,7 +6,6 @@ namespace UI
 {
     public class PlayerLabel : MonoBehaviour
     {
-        [System.Obsolete]
         public void DrawLabel(Camera camera)
         {
             if (camera == null)
@@ -26,7 +25,8 @@ namespace UI
                 var collider = obj.GetComponent<Collider>();
                 if (collider != null && camera.Visible(collider) && obj.transform != transform)
                 {
-                    GUI.Label(new Rect(new Vector2(position.x,Screen.height - position.y), new Vector2(10, name.Length * 10.5f )), obj.name, style);
+                    GUI.Label(new Rect(new Vector2(position.x,Screen.height - position.y), new Vector2(10, name.Length * 10.5f )),
+                        obj.name, style);
                 }
             }
         }
