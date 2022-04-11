19586dc1983701153bf8c6c95c4b35f63d8b9ff3
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-04-11 21:15:31 +0600

    Подготовил материал к заданию из лекции 8

---
+++ Assets/Scripts/CustomRP/SpaceRunPipelineRenderAsset.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomRenderPipeline
+{
+    [CreateAssetMenu(menuName = "Rendering/SpaceRunPipelineRenderAsset")]
+    public class SpaceRunPipelineRenderAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new SpaceRunPipelineRender();
+        }
+    }
+}
